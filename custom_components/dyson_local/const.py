76beeb2281394583f2d4c9f31c4b38205abf8ce0
Sam Soffes <sam@soff.es>
2023-05-29 10:27:19 -0700

    Delete Forest home

--- custom_components/dyson_local/const.py
+++
@@ -1,9 +0,0 @@
-DOMAIN = "dyson_local"
-
-CONF_SERIAL = "serial"
-CONF_CREDENTIAL = "credential"
-CONF_DEVICE_TYPE = "device_type"
-
-DATA_DEVICES = "devices"
-DATA_DISCOVERY = "discovery"
-DATA_COORDINATORS = "coordinators"
